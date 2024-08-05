29675e45ecb46384e166cd18063dffde8d6f63e9
si-jaideep-singh <jaideep.singh@sportzinteractive.net>
2024-08-05 12:27:09 +0530

    Project Setup

---
+++ HomePage_Demo/HomePage_Demo/HomePage_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomePage_DemoApp.swift
+//  HomePage_Demo
+//
+//  Created by Jaideep Singh on 05/08/24.
+//
+
+import SwiftUI
+
+@main
+struct HomePage_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
